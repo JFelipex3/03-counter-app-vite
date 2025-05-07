b2991a9c6c3e21f5c737403047a6fb0b0e9762dc
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-05-07 18:16:32 -0400

    Uso de Fragmento para agrupar elementos HTML

---
+++ src/FirstApp.jsx
@@ -0,0 +1,9 @@
+export const FirstApp = () => {
+    return (
+        // Definición de un fragmento sin necesidad de importar
+        <>
+            <h1>First App</h1> 
+            <p>Soy un subtitulo</p>
+        </>
+    );
+}
