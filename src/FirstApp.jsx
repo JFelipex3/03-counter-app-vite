ab1be5d0e03b409e3233090c673b8bed0d8f415c
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-05-08 10:34:48 -0400

    Impresión de variables y colocar stilos de CSS global

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,24 @@
+// Ponerlo fuera no se renderiza nuevamente y está encapsulado en el componente
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'Soy un título'
+};
+
+// Función que retona saludo y no depende de funcionamiento interno de FirstApp
+const getSaludo = () => {
+    return `Hola, Jhonnatan`;
+}
+
 export const FirstApp = () => {
+
+    // Si una función depende de valores de dentro de esta función se debe declarar acá.
+
     return (
         // Definición de un fragmento sin necesidad de importar
         <>
-            <h1>First App</h1> 
+            <code>{ JSON.stringify( newMessage ) }</code>
+            {/* No puede ser una función async, ni una promesa */}
+            <h1> { getSaludo() } </h1>
             <p>Soy un subtitulo</p>
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelloWorldApp } from './HelloWorldApp';
+
+// import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
+// importar styles global para toda la app
+import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
